9b9b0894a8c3c27d49f2403d0596709ae4ac2897
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-16 22:08:01 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,10 +4,13 @@ class CityRepository {
 
     async createCity({ name }){
         try {
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name
+            });
             return city;
 
         } catch (error) {
+            console.log("Something went wrong in the repository layer!");
             throw {error};
         }
     }
@@ -20,9 +23,36 @@ class CityRepository {
                 }
             });
         } catch (error) {
+            console.log("Something went wrong in the repository layer!");
             throw {error};
         }
     }
+
+    async updateCity(data, cityId) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            })
+            return city;
+            
+        } catch (error) {
+            console.log("Something went wrong in the repository layer!");
+            throw {error};
+        }
+    }
+
+    async getCity({ cityId }) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer!");
+            throw {error};
+        }
+    }
+    
 }
 
 module.exports = CityRepository;
