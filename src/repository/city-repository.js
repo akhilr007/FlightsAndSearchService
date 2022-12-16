dd0739d780e498a46873726446dbcc9322e24b64
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-17 00:26:12 +0530

    fix city-repository delete city bug

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -15,7 +15,7 @@ class CityRepository {
         }
     }
 
-    async deleteCity({ cityId }){
+    async deleteCity(cityId){
         try {
             await City.destroy({
                 where: {
